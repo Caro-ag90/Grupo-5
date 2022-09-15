3d240bf31e0bfa0c55b1c1c0baf97354295444c0
Caro-ag90 <cacunag@unicaragena.edu.co>
2022-09-14 21:11:56 -0500

    decoradores de spring para convertir en api

--- src/main/java/borrador/pg5/App.java
+++ src/main/java/borrador/pg5/App.java
@@ -1,31 +1,15 @@
 package borrador.pg5;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
-import borrador.pg5.controlador.EstudiantesController;
-import borrador.pg5.modelo.Estudiantes;
-
-/**
- * Hello world!
- *
- */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );    
-        
-        /* List<Estudiantes> estudiante = new EstudiantesController().obtenerXid("001"); 
-        for (int i = 0; i < estudiante.size(); i++) {
-            System.out.println(estudiante.get(i));
-        } */
-
-        Estudiantes estudiante = new EstudiantesController().getXid("001"); 
-        
-        System.out.println(estudiante.toString());
-        
-
-        
+        SpringApplication.run(App.class, args);        
 
     }
+    
 }
--- src/main/java/borrador/pg5/controlador/EstudiantesController.java
+++ src/main/java/borrador/pg5/controlador/EstudiantesController.java
@@ -6,9 +6,14 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import borrador.pg5.modelo.Estudiantes;
 
+@RestController
+@RequestMapping("/estudiantes")
 public class EstudiantesController {
     private SessionFactory factory;
     
@@ -22,9 +27,21 @@ public class EstudiantesController {
         session.beginTransaction();
         return session;
       }      
+@GetMapping     
+public List<Estudiantes> obtenerEstudiantes(){
+    List<Estudiantes> mascotas = new ArrayList<>();    
+    Session session = openSession();
+    try {
+      mascotas = session.createQuery("from Estudiantes", Estudiantes.class).list();
+    } catch (Exception e) {
+      e.printStackTrace();
       
+    }
+    session.close();
+    return mascotas;
+}
 
-      public List<Estudiantes> obtenerXid(String id){
+public List<Estudiantes> obtenerXid(String id){
         List<Estudiantes> mascotas = new ArrayList<>();    
         Session session = openSession();
         try {
