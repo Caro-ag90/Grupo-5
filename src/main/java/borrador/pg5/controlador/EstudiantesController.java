9e3b86b6296bedd56b54c26be97a0d16723dc603
Caro-ag90 <cacunag@unicaragena.edu.co>
2022-09-16 19:30:02 -0500

    agregando carpeta servicio

--- src/main/java/borrador/pg5/controlador/EstudiantesController.java
+++ src/main/java/borrador/pg5/controlador/EstudiantesController.java
@@ -1,72 +1,37 @@
 package borrador.pg5.controlador;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import borrador.pg5.modelo.Estudiantes;
+import borrador.pg5.servicio.EstudiantesService;
 
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudiantesController {
-    private SessionFactory factory;
+  EstudiantesService servicio;    
     
     public EstudiantesController() {
-        factory = new Configuration().configure("cfg.xml").addAnnotatedClass(Estudiantes.class).buildSessionFactory();
+      servicio= new EstudiantesService();
 
     }
 
-    public Session openSession() {
-        Session session = factory.openSession();
-        session.beginTransaction();
-        return session;
-      }      
+        
 @GetMapping     
 public List<Estudiantes> obtenerEstudiantes(){
-    List<Estudiantes> mascotas = new ArrayList<>();    
-    Session session = openSession();
-    try {
-      mascotas = session.createQuery("from Estudiantes", Estudiantes.class).list();
-    } catch (Exception e) {
-      e.printStackTrace();
-      
-    }
-    session.close();
-    return mascotas;
+   return servicio.obtenerEstudiantes();
 }
 
-public List<Estudiantes> obtenerXid(String id){
-        List<Estudiantes> mascotas = new ArrayList<>();    
-        Session session = openSession();
-        try {
-          mascotas = session.createQuery("from Estudiantes where idEstudiante = :param_id", Estudiantes.class).setParameter("param_id", id).list();
-        } catch (Exception e) {
-          e.printStackTrace();
-          
-        }
-        session.close();
-        return mascotas;
-      }
 
-      public Estudiantes getXid(String id){
-        Estudiantes estu = new Estudiantes();    
-        Session session = openSession();
-        try {
-          estu = session.createQuery("from Estudiantes where idEstudiante = :param_id", Estudiantes.class).setParameter("param_id", id).getSingleResult();
-        } catch (Exception e) {
-          e.printStackTrace();
-          
-        }
-        session.close();
-        return estu;
-      }
-    
+
+@GetMapping("/{id}")  
+public Estudiantes getXid(@PathVariable(name="id") String id){
+  return servicio.getXid(id);
+}     
 
     
 }
---
+++ src/main/java/borrador/pg5/servicio/EstudiantesService.java
@@ -0,0 +1,64 @@
+package borrador.pg5.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import borrador.pg5.modelo.Estudiantes;
+
+public class EstudiantesService {
+    private SessionFactory factory;
+
+    public EstudiantesService() {
+        factory = new Configuration().configure("cfg.xml").addAnnotatedClass(Estudiantes.class).buildSessionFactory();
+    }
+
+    public Session openSession() {
+        Session session = factory.openSession();
+        session.beginTransaction();
+        return session;
+      }  
+    
+      public List<Estudiantes> obtenerEstudiantes(){
+        List<Estudiantes> mascotas = new ArrayList<>();    
+        Session session = openSession();
+        try {
+          mascotas = session.createQuery("from Estudiantes", Estudiantes.class).list();
+        } catch (Exception e) {
+          e.printStackTrace();
+          
+        }
+        session.close();
+        return mascotas;
+    }
+
+    public List<Estudiantes> obtenerXid(String id){
+        List<Estudiantes> mascotas = new ArrayList<>();    
+        Session session = openSession();
+        try {
+          mascotas = session.createQuery("from Estudiantes where idEstudiante = :param_id", Estudiantes.class).setParameter("param_id", id).list();
+        } catch (Exception e) {
+          e.printStackTrace();
+          
+        }
+        session.close();
+        return mascotas;
+      }
+
+      public Estudiantes getXid(String id){
+        Estudiantes estu = new Estudiantes();    
+        Session session = openSession();
+        try {
+          estu = session.createQuery("from Estudiantes where idEstudiante = :param_id", Estudiantes.class).setParameter("param_id", id).getSingleResult();
+        } catch (Exception e) {
+          e.printStackTrace();
+          
+        }
+        session.close();
+        return estu;
+      }
+    
+}
